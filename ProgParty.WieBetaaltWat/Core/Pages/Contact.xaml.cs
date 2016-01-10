ba8550e495664fb03e67b00dcb8b21edd04aa554
Jens den Braber <jens.denbraber@gmail.com>
2016-01-10 22:15:44 +0100

    core folder added

---
+++ ProgParty.WieBetaaltWat/Core/Pages/Contact.xaml.cs
@@ -0,0 +1,112 @@
+﻿using ProgParty.Core.Review;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProgParty.Core.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Contact : Page
+    {
+        public Contact()
+        {
+            this.InitializeComponent();
+
+            SetFooterImage();
+
+            Core.Track.Telemetry.Instance.PageVisit(this);
+        }
+
+        public Contact(SolidColorBrush foregroundColor, SolidColorBrush backgroundColor, bool imageMirrored = false)
+        {
+            this.InitializeComponent();
+
+            SetColors(foregroundColor, backgroundColor);
+            SetFooterImage(imageMirrored);
+
+            Core.Track.Telemetry.Instance.PageVisit(this);
+        }
+
+
+        private void SetColors(SolidColorBrush foregroundColor, SolidColorBrush backgroundColor)
+        {
+            this.Background = backgroundColor;
+            this.Foreground = foregroundColor;
+        }
+
+        private void SetFooterImage(bool imageMirrored = false)
+        {
+            BitmapImage footerImage = new BitmapImage();
+            
+            if (imageMirrored)
+                footerImage.UriSource = new Uri("ms-appx:///Core/Assets/foto_jens_dennis_mirror.png");
+            else
+                footerImage.UriSource = new Uri("ms-appx:///Core/Assets/foto_jens_dennis.png");
+
+            FooterImage.Source = footerImage;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+#if WINDOWS_PHONE_APP
+            Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+#endif
+        }
+
+#if WINDOWS_PHONE_APP
+        void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                e.Handled = true;
+                Frame.GoBack();
+            }
+        }
+#endif
+        
+        private async void ButtonReview_Click(object sender, RoutedEventArgs e)
+        {
+            Core.Track.Telemetry.Instance.ReviewButtonClicked();
+            await Review.Review.Instance.Execute();
+        }
+
+        private void FotoJensDennis_Loaded(object sender, RoutedEventArgs e)
+        {
+            FooterImage.Width = Window.Current.Bounds.Width;
+        }
+
+        private async void DennisMail_Click(object sender, RoutedEventArgs e)
+        {
+#if WINDOWS_PHONE_APP
+            //predefine Recipient
+            Windows.ApplicationModel.Email.EmailRecipient sendTo = new Windows.ApplicationModel.Email.EmailRecipient()
+            {
+                Address = "dennis.rosenbaum@outlook.com"
+            };
+            Windows.ApplicationModel.Email.EmailRecipient sendCc = new Windows.ApplicationModel.Email.EmailRecipient()
+            {
+                Address = "jens.denbraber@gmail.com"
+            };
+
+            //generate mail object
+            Windows.ApplicationModel.Email.EmailMessage mail = new Windows.ApplicationModel.Email.EmailMessage();
+            mail.Subject = "Hi Prog Party!";
+            mail.Body = "You are great";
+
+            //add recipients to the mail object
+            mail.To.Add(sendTo);
+            mail.CC.Add(sendCc);
+
+            //open the share contract with Mail only:
+            await Windows.ApplicationModel.Email.EmailManager.ShowComposeNewEmailAsync(mail);
+#endif
+        }
+    }
+}
---
+++ ProgParty.WieBetaaltWat/Core/Pages/Shop.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Windows.ApplicationModel.Store;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using ProgParty.Core;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProgParty.Core.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Shop : Page
+    {
+        public Shop()
+        {
+            this.InitializeComponent();
+            SetRemoveAdvertisements();
+            SetDonations();
+        }
+        
+        private async void BuyRemoveAds_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Config.Instance.LicenseInformation.ProductLicenses[InAppPurchase.TokenRemoveAdvertisement].IsActive)
+            {
+                try
+                {
+                // show the purchase dialog.
+#if DEBUG
+                await CurrentAppSimulator.RequestProductPurchaseAsync(InAppPurchase.TokenRemoveAdvertisement);
+#else
+                await CurrentApp.RequestProductPurchaseAsync(InAppPurchase.TokenRemoveAdvertisement);
+#endif
+                SetRemoveAdvertisements();
+            }
+            catch (Exception)
+                {
+                    // The in-app purchase was not completed because 
+                    // an error occurred.
+                }
+            }
+            else
+            {
+                SetRemoveAdvertisements();
+            }
+        }
+
+        private void SetRemoveAdvertisements()
+        {
+            if (Config.Instance.LicenseInformation.ProductLicenses[InAppPurchase.TokenRemoveAdvertisement].IsActive)
+            {
+                RemoveAdsButton.Visibility = Visibility.Collapsed;
+                RemoveAdsPrice.Visibility = Visibility.Collapsed;
+                RemoveAdsBought.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                RemoveAdsBought.Visibility = Visibility.Collapsed;
+            }
+        }
+                
+        private async void Donation_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Config.Instance.LicenseInformation.ProductLicenses[InAppPurchase.TokenDonation].IsActive)
+            {
+                try
+                {
+                    // show the purchase dialog.
+#if DEBUG
+                    await CurrentAppSimulator.RequestProductPurchaseAsync(InAppPurchase.TokenDonation);
+#else
+                    await CurrentApp.RequestProductPurchaseAsync(InAppPurchase.TokenDonation);
+#endif
+                    SetDonations();
+                }
+                catch (Exception)
+                {
+                    // The in-app purchase was not completed because 
+                    // an error occurred.
+                }
+            }
+            else
+            {
+                SetDonations();
+            }
+        }
+
+        private void SetDonations()
+        {
+            if (Config.Instance.LicenseInformation.ProductLicenses[InAppPurchase.TokenDonation].IsActive)
+            {
+                DonationButton.Visibility = Visibility.Collapsed;
+                DonationPrice.Visibility = Visibility.Collapsed;
+                DonationBought.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                DonationBought.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
